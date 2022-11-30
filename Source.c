05ed7fd89d481202a675d214b52e3326ed9fc4f6
DimTaehyeon <DimTaehyeon@users.noreply.github.com>
2022-11-30 11:28:31 +0900

    123123

--- Source.c
+++ Source.c
@@ -1,16 +1,30 @@
 ﻿#include <stdio.h>
-#include <time.h> // 세탁기 작동시간은 50분이니 (1)50분 카운트다운 (2)현재시간 + 50분해서 작동종료시간 표기, 참조링크 https://url.kr/idm58x
-#include <windows.h>
-#include <conio.h> // 방향키 입력 받는 헤더파일, 참조링크 https://url.kr/8rk6ng, 네이버 블로그
+#include <time.h> // 세탁기 작동시간은 50분이니 50분 카운트다운 참조링크 https://url.kr/idm58x
+#include <windows.h> 
+#include <conio.h> // 방향키 입력 받는 헤더파일, 참조링크 https://url.kr/8rk6ng, 
+//_getch _kbhit 설명 <= https://url.kr/w5azx8 네이버 블로그
+// 총 7층, 세탁기(50분) 3대, 건조기(50분) 1대
 #define LEFT 75
 #define RIGHT 77
 #define UP 72
 #define DOWN 80
 
-int main(void) {
-	int num;
-	char select;
+void CursorView() {
+	CONSOLE_CURSOR_INFO cursorInfo = { 0, };
+	cursorInfo.dwSize = 1; //커서 굵기 (1 ~ 100)
+	cursorInfo.bVisible = FALSE; //커서 Visible TRUE(보임) FALSE(숨김)
+	SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &cursorInfo);
+}
 
+void fullscreen() {
+	SetConsoleDisplayMode(GetStdHandle(STD_OUTPUT_HANDLE), CONSOLE_FULLSCREEN_MODE, 0);
+}
+
+int main(void) {
+	int num; // 숫자 입력
+	char select; // 방향키 입력
+	fullscreen();
+	CursorView(0);
 	// 방향키로 메뉴 입력을 받는게 좋을 것 같음, 우선 번호입력 / 방향키 입력폼을 두개다 해둠.
 	printf("■■■■■■■■■■■■■■■■■\n");
 	printf("■                              ■\n");
@@ -18,25 +32,28 @@ int main(void) {
 	printf("■   (1)세탁기 이용현황 조회    ■\n");
 	printf("■   (2)세탁기 이용현황 등록    ■\n");
 	printf("■                              ■\n");
-	printf("■ (←)이용현황 조회            ■\n");
-	printf("■            이용현황 등록(→) ■\n");
-	printf("■      방향키를 통해 선택      ■\n");
+	printf("■     번호입력을 통해 선택     ■\n");
 	printf("■                              ■\n");
 	printf("■■■■■■■■■■■■■■■■■\n");
 
-	/* printf("선택 : ");
-	scanf_s("%d", &num);
-	
+	num = _getch();
+
 	switch (num) {
-	case 1:
+	case '1':
 		inquiry();
 		break;
-	case 2:
+	case '2':
 		registeration();
 		break;
-	}  */
+	default:
 
-	while (1) {
+		break;
+	}
+
+	return 0;
+}
+
+	/*while (1) {
 		if (_kbhit()) {
 			select = _getch();
 			if (select == -32) {
@@ -53,9 +70,9 @@ int main(void) {
 		}
 	}
 	return 0;
-}
+}*/
 
-int inquiry() { //조회
+	int inquiry() { //조회
 	system("cls");
 	printf("이용시설 현황 조회을 선택하셨습니다.\n");
 	
@@ -66,7 +83,7 @@ int inquiry() { //조회
 	return 0; 
 }
 
-int registeration() { //등록
+	int registeration() { //등록
 	system("cls");
 	printf("이용시설 현황 등록을 선택하셨습니다.\n");
 	
